1ed1306fe3abee0909bc3420158214d8dfbed16c
SteveTuttle <127702378+SteveTuttle@users.noreply.github.com>
2024-03-04 19:04:32 -0500

    logic one up

--- static/js/logic_1.js
+++ static/js/logic_1.js
@@ -5,32 +5,39 @@ console.log("Start creating Map, use logic_1");
 
 // Create the base layers.
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 })
 
 let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
 // Create a baseMaps object.
 let baseMaps = {
-"Street Map": street,
-"Topographic Map": topo
+    "Street Map": street,
+    "Topographic Map": topo
 };
 
-// Create an empty leaflet layergroup for earthquakes
+// Create a new empty Leaflet layergroup for earthquakes.
 let earthquakes = new L.layerGroup();
 
 // Create an overlay object to hold the overlay.
 let overlayMaps = {
-Earthquakes: earthquakes
+    Earthquakes: earthquakes
 };
 
 // Create the map, giving it the streetmap and earthquakes layers to display on load.
 let myMap = L.map("map", {
-center: [
-    37.09, -95.71
-],
-zoom: 5,
-layers: [street, earthquakes]
+    center: [
+        37.09, -95.71
+    ],
+    zoom: 3.75,
+    layers: [street, earthquakes]
 });
+
+// Create a layer control.
+// Pass it our baseMaps and overlayMaps.
+// Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(myMap);
